711ee23c7cbdc17cea763e31986c68cb0fbd429b
xavimurtagh <90108226+xavimurtagh@users.noreply.github.com>
2025-04-03 22:54:43 +0100

    Update app.py

--- bask project website/website/app.py
+++ bask project website/website/app.py
@@ -27,6 +27,7 @@ Session(app)
 if __name__ == "__main__":
     with app.test_request_context("/"):
         session["key"] = "value"
+        app.run(host="0.0.0.0", port=8000)
 
 
 #conn = sqlite3.connect('players.db')
